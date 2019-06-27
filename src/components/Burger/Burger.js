d3e8428b1715ef518609bc6bded076c99ef72223
Kyle Bustard <bustardkyle@gmail.com>
2019-06-27 09:12:19 -0500

    Add prop types and make BurgerIngredients component stateful

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import BurgerIngredients from './BurgerIngredients/BurgerIngredients';
+
+const burger = () => {
+  return ();
+}
+
+export default burger;
---
+++ src/components/Burger/BurgerIngredients/BurgerIngredients.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classes from './BurgerIngredients.css';
+
+class BurgerIngredients extends Component {
+  render() {
+    return ();
+  }
+}
+
+export default BurgerIngredients;

d0825fefacc634f930052a91a55b638d30f0db5e
Kyle Bustard <bustardkyle@gmail.com>
2019-06-26 21:45:21 -0500

    Add BurgerBuild component and some styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
-function App() {
-  return (
-    <Layout>
-      <p>Test text</p>
-    </Layout>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;

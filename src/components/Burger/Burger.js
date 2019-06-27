59e87cf2174b60f4e75a6ed75d84cc7fcc30bf81
Kyle Bustard <bustardkyle@gmail.com>
2019-06-27 14:58:09 -0500

    Add prompt to build burger if ingredient count is zero

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,19 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.css';
 
 const burger = (props) => {
-  const transformedIngredients =
-    Object.keys(props.ingredients)
-      .map(igKey => {
-        return [...Array(props.ingredients[igKey])].map((_, i )=> {
-          return <BurgerIngredient key={igKey + i} type={igKey} />
-        });
-      });
+  let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+    return [...Array(props.ingredients[igKey])].map((_, i) => {
+      
+      return <BurgerIngredient key={igKey + i} type={igKey} />
+    });
+  })
+  .reduce((arr, el) => {
+    return arr.concat(el);
+  }, []);
+console.log(transformedIngredients);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
 
   return (
     <div className={classes.Burger}>

766929004abbb0832380a356487eecf95e500d08
Kyle Bustard <bustardkyle@gmail.com>
2019-06-27 12:18:52 -0500

    Add burger styling

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import BurgerIngredients from './BurgerIngredients/BurgerIngredients';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import classes from './Burger.css';
 
 const burger = () => {
-  return ();
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      <BurgerIngredient type="cheese" />
+      <BurgerIngredient type="meat" />
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
 }
 
 export default burger;

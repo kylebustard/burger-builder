c59e4ff575f15e70e322e4ba285ce8cafccc1b03
Kyle Bustard <bustardkyle@gmail.com>
2019-06-27 16:36:14 -0500

    Fix Build Controls style

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' },
+];
 
 const buildControls = props => {
   return (
-  <div>
-
-  </div>
-  )
+    <div className={classes.BuildControls}>
+      {controls.map(ctrl => {
+        return <BuildControl key={ctrl.label} label={ctrl.label} />;
+      })}
+    </div>
+  );
 }
 
 export default buildControls;

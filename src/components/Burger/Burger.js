07fd29ec8d3606c5a930fdc7d97dc9bf74ea830b
Kyle Bustard <bustardkyle@gmail.com>
2019-06-27 20:39:34 -0500

    Add `remove ingredient button`

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,14 +5,14 @@ import classes from './Burger.css';
 const burger = (props) => {
   let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
     return [...Array(props.ingredients[igKey])].map((_, i) => {
-      
+
       return <BurgerIngredient key={igKey + i} type={igKey} />
     });
   })
-  .reduce((arr, el) => {
-    return arr.concat(el);
-  }, []);
-console.log(transformedIngredients);
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
   if (transformedIngredients.length === 0) {
     transformedIngredients = <p>Please start adding ingredients!</p>;
   }
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -30,23 +30,29 @@ class BurgerBuilder extends Component {
     const priceAddition = INGREDIENT_PRICES[type];
     const oldPrice = this.state.totalPrice;
     const newPrice = oldPrice + priceAddition;
-    this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
   }
 
   removeIngredientHandler = type => {
     const oldCount = this.state.ingredients[type];
-    const updatedCount = oldCount + 1;
+    const updatedCount = oldCount - 1;
     const updatedIngredients = {
       ...this.state.ingredients
     }
     updatedIngredients[type] = updatedCount;
+    const priceDeduction = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceDeduction;
+    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
   }
 
   render() {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <BuildControls ingredientAdded={this.addIngredientHandler} />
+        <BuildControls
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredientHandler} />
       </Aux>
     );
   }

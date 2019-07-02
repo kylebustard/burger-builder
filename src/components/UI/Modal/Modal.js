342839001234f27f55507abb59994778806e8e87
Kyle Bustard <bustardkyle@gmail.com>
2019-07-02 14:25:22 -0500

    Add toggle button

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -8,14 +8,13 @@ const modal = props => {
   return (
     <Aux>
       <Backdrop show={props.show} clicked={props.modalClosed} />
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
-      }}>
-      {props.children}
-    </div>
+      <div
+        className={classes.Modal}
+        style={{
+          transform: props.show ? 'translateY(0)' : 'translateY(-100vh)', opacity: props.show ? '1' : '0'
+        }}>
+        {props.children}
+      </div>
     </Aux>
   );
 }

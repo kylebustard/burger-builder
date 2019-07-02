c6bfde12da1faa8748b28abe45a8762b57371edf
Kyle Bustard <bustardkyle@gmail.com>
2019-07-02 16:35:52 -0500

    Change file structure: move `Layout/` from `components/` to `hoc/`

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import Aux from '../../../hoc/Aux';
+import React, { Component } from 'react';
+import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = props => {
-  const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+  componentWillUpdate() {
+    console.log('[OrderSummary] WillUpdate');
+  }
+
+  render() {
+  const ingredientSummary = Object.keys(this.props.ingredients)
     .map(igKey => {
       return (
         <li key={igKey}>
           <span style={{ textTransform: 'capitalize' }}>
             {igKey}
           </span>
-          : {props.ingredients[igKey]}
+          : {this.props.ingredients[igKey]}
         </li>
       );
     })
+
   return (
     <Aux>
       <h3>Your Order</h3>
@@ -21,12 +27,13 @@ const orderSummary = props => {
       <ul>
         {ingredientSummary}
       </ul>
-      <p><strong>Totoal Price: {props.price.toFixed(2)}</strong></p>
+      <p><strong>Totoal Price: {this.props.price.toFixed(2)}</strong></p>
       <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+      <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+      <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
     </Aux>
   );
+  }
 }
 
-export default orderSummary;
+export default OrderSummary;

2f28a73105a1ae791151acde000a189025d96a8e
Kyle Bustard <bustardkyle@gmail.com>
2019-07-02 11:27:07 -0500

    Add navigation for toolbar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import classes from './Toolbar.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = props => {
   return (
     <header className={classes.Toolbar}>
       <div>Menu</div>
-      <div>Logo</div>
+      <Logo />
       <nav>
-        ...
+        <NavigationItems />
     </nav>
     </header>
   );
